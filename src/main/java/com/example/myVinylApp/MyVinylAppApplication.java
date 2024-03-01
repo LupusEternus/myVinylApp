9a89f78c918c6a0e5fb074749014b9b117fb7542
LupusEternus <wilk.tomek@gamil.com>
2024-03-01 16:41:58 +0100

    Spring project creation

---
+++ src/main/java/com/example/myVinylApp/MyVinylAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.myVinylApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyVinylAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyVinylAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/myVinylApp/MyVinylAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.myVinylApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyVinylAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
